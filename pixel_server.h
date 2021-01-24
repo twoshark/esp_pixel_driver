6b4ffff921303a8a815ae563f1ecf73d3875c4a9
Tushar Shah <twoshark@gmail.com>
2021-01-24 11:39:32 -0500

    DNSServer only for 8266

--- pixel_server.h
+++ pixel_server.h
@@ -3,12 +3,12 @@
 #ifdef ESP8266
 #include <esp8266wifi.h>
 #include <ESP8266WebServer.h>
+#include <DNSServer.h>
 #elif defined(ESP32)
 #include <WiFi.h>
 #include <WebServer.h>
 #endif
 
-#include <DNSServer.h>
 #include "config.h"
 
 class PixelServer
